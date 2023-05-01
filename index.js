54bb42eaf6119a3de42b5c65bf78734f5a919a55
Daniel <toomey.d.p@gmail.com>
2023-05-01 16:27:55 -0500

    Add javascript variables
    Add variables for player, computer and result display elements, variables for the three choice buttons, and variables for the player, compueter, and the result

---
+++ index.js
@@ -0,0 +1,7 @@
+const playerText = document.querySelector("#player-text");
+const computerText = document.querySelector("#computer-text");
+const resultText = document.querySelector("#result-text");
+const choiceBtns = document.querySelector(".choice-button");
+let player;
+let computer;
+let result;
