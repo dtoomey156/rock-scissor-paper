709a05417fcc762c827747139e71afd224cda928
Daniel <toomey.d.p@gmail.com>
2023-05-01 20:27:16 -0500

    Implement game JS logic
    Add event listener to each of choice buttons to store choice to player variable. Add random number function to computer turn and assign one rock, paper, scissor. Add check winner function using ternary conditional to check outcome of match.

--- index.js
+++ index.js
@@ -1,7 +1,43 @@
 const playerText = document.querySelector("#player-text");
 const computerText = document.querySelector("#computer-text");
 const resultText = document.querySelector("#result-text");
-const choiceBtns = document.querySelector(".choice-button");
+const choiceBtns = document.querySelectorAll(".choice-button");
 let player;
 let computer;
 let result;
+
+choiceBtns.forEach(button => button.addEventListener("click", () => {
+    player = button.textContent;
+    computerTurn();
+    playerText.textContent = `Player: ${player}`;
+    computerText.textContent = `Computer: ${computer}`;
+    resultText.textContent = checkWinner();
+} ));
+
+function computerTurn() {
+    const randNum = Math.floor(Math.random() * 3 ) + 1;
+
+    switch(randNum) {
+        case 1:
+            computer = "Rock";
+            break;
+        case 2:
+            computer = "Paper";
+            break;
+        case 3:
+            computer = "Scissors";
+            break;
+    }
+}
+
+function checkWinner() {
+    if(player == computer) {
+        return "Draw!";
+    } else if (computer == "Rock") {
+        return (player == "Paper") ? "You win!" : "You Lose!"
+    } else if (computer == "Paper") {
+        return (player == "Scissors") ? "You win!" : "You Lose!"
+    } else if (computer == "Scissors") {
+        return (player == "Rock") ? "You Win!" : "You Lose!"
+    }
+}
